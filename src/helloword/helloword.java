8c5418b990ff6ad6c8c9ea02aa5f7e22624774ff
Mr .F <49602070+xinleif@users.noreply.github.com>
2020-05-08 22:47:11 +0800

    Update helloword.java

--- src/helloword/helloword.java
+++ src/helloword/helloword.java
@@ -4,6 +4,7 @@ public class helloword {
 	
 	public static void main(String[] args) {
 		System.out.println("HelloWord!HelloWord!修改一下");
+                System.out.println("HelloWord!HelloWord!修改一下");
 	}
 
 }
